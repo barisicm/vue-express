51b80869ccc904899754e5dc4b67d312d81ccc2a
barisicm <barisic.m@outlook.com>
2019-08-10 02:21:27 +0200

    Bootstrap and axios setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
+import BoostrapVue from "bootstrap-vue"
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import './assets/style.scss'
+
 Vue.config.productionTip = false
+
+Vue.use(BoostrapVue);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
