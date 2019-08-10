17068de79beea6f5671e64317b7e970dc937fcb9
barisicm <barisic.m@outlook.com>
2019-08-10 04:14:49 +0200

    Added sending fake bearer token

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-      <h1>Welcome!</h1>
+      <div v-if="loading">Loading ...</div>
+      <div v-if="!loading && !error">Welcome</div>
+      <div v-if="!loading && error">Something went terribly wrong</div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
     export default {
-        name: "App"
+        name: "App",
+
+        data() {
+            return {
+                loading: true,
+                error: false
+            }
+        },
+
+        methods: {
+            ...mapActions(['initialLoad'])
+        },
+
+        async mounted() {
+            try {
+                await this.initialLoad();
+                this.loading = false;
+            } catch (error) {
+                this.loading = false;
+                this.error = true;
+            }
+        }
     };
 </script>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,26 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: {
+      id: null,
+      email: "",
+      username: ""
+    }
   },
   mutations: {
-
+    CURRENT_USER_FETCHED(state, user) {
+      state.user.id = user.id;
+      state.user.email = user.email;
+      state.user.username = user.username;
+    }
   },
   actions: {
-
+    async initialLoad(context) {
+      if(localStorage.bgtrackerjwt){
+        Vue.axios.defaults.headers.common.Authorization = `Bearer ${localStorage.bgtrackerjwt}`;
+        const res = await Vue.axios.get("/api/auth/currentUser");
+        context.commit("CURRENT_USER_FETCHED", res.data.user);
+      }
+    }
   }
 })
