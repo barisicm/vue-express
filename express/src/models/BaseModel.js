835bb9afba20c21a31f2bafcc0e33d7471abe060
Marin Barišić <marin.barisic.external@t.ht.hr>
2019-08-29 09:38:20 +0200

    Added fake middleware, added vue proxy, added objection library

---
+++ express/src/models/BaseModel.js
@@ -0,0 +1,16 @@
+const { Model } = require('objection')
+
+class BaseModel extends Model {
+    $beforeInsert(){
+        const now = new Date().toISOString();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    $beforeUpdate(){
+        const now = new Date().toISOString();
+        this.updatedAt = now;
+    }
+}
+
+module.exports = BaseModel
